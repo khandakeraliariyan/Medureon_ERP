5aba4fa4cf4e090f07af97960c1e8f89f89d1d72
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-07 13:25:35 +0600

    added some function in the auth service

--- server/src/modules/auth/auth.service.js
+++ server/src/modules/auth/auth.service.js
@@ -87,7 +87,99 @@ const loginUser =
         };
     };
 
+const getMe =
+    async (userId) => {
+
+        return await User.findById(
+            userId
+        ).select("-password");
+
+    };
+
+const changePassword =
+    async (
+        userId,
+        oldPassword,
+        newPassword
+    ) => {
+
+        const user =
+            await User.findById(
+                userId
+            ).select("+password");
+
+        const matched =
+            await bcrypt.compare(
+                oldPassword,
+                user.password
+            );
+
+        if (!matched) {
+            throw new Error(
+                "Wrong password"
+            );
+        }
+
+        user.password =
+            await bcrypt.hash(
+                newPassword,
+                10
+            );
+
+        await user.save();
+
+        return true;
+    };
+
+const updateProfile =
+    async (
+        userId,
+        payload
+    ) => {
+
+        return await User.findByIdAndUpdate(
+            userId,
+            payload,
+            {
+                new: true,
+            }
+        );
+    };
+
+const getAllUsers =
+    async () => {
+
+        return await User.find()
+            .select("-password")
+            .sort({
+                createdAt: -1,
+            });
+
+    };
+
+const updateRole =
+    async (
+        userId,
+        role
+    ) => {
+
+        return await User.findByIdAndUpdate(
+            userId,
+            {
+                role,
+            },
+            {
+                new: true,
+            }
+        );
+    };
+
 module.exports = {
     registerUser,
     loginUser,
+    getMe,
+    changePassword,
+    updateProfile,
+    getAllUsers,
+    updateRole
 };
