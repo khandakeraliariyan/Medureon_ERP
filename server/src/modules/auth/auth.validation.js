b705276341b1786bcf81ee7ef361a542801e8d56
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-07 13:04:24 +0600

    added the auth validator

---
+++ server/src/modules/auth/auth.validation.js
@@ -0,0 +1,43 @@
+const { z } =
+    require("zod");
+
+const registerSchema =
+    z.object({
+        body: z.object({
+            name: z
+                .string()
+                .min(3),
+
+            email: z
+                .string()
+                .email(),
+
+            password: z
+                .string()
+                .min(6),
+
+            role: z.enum([
+                "admin",
+                "manager",
+                "cashier",
+                "inventory",
+            ]),
+        }),
+    });
+
+const loginSchema =
+    z.object({
+        body: z.object({
+            email: z
+                .string()
+                .email(),
+
+            password:
+                z.string(),
+        }),
+    });
+
+module.exports = {
+    registerSchema,
+    loginSchema,
+};
