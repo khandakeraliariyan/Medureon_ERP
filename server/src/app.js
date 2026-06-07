99543cbcac07d6ecbf1d2803955a35d209afebef
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-07 13:17:31 +0600

    added global error handler and connected with app

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
+const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
+app.use(errorHandler);
 
 app.use(cors());
 
 app.use(helmet());
 
 app.use(morgan("dev"));
+
+app.use("/api/v1", routes);
 
 app.get("/", (req, res) => {
     res.json({
---
+++ server/src/middleware/errorHandler.js
@@ -0,0 +1,18 @@
+const errorHandler =
+    (
+        err,
+        req,
+        res,
+        next
+    ) => {
+
+        res.status(400).json({
+            success: false,
+            message:
+                err.message ||
+                "Something went wrong",
+        });
+    };
+
+module.exports =
+    errorHandler;
